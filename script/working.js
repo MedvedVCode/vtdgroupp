10f0a71cd7cc9eebf3dc8df380de382b8fab06b6
Fedorov Denis <ssboshen@yandex.ru>
2024-06-22 21:13:15 +0300

    refactor work time in heager

--- script/working.js
+++ script/working.js
@@ -1,18 +1,25 @@
-const DAY = 24;
-const HOUR = 60;
-const MINUTE = 60;
 const SECOND = 1000;
+const MINUTE = 60 * SECOND;
+
+const MOSCOW_TIME_ZONE = -180; //в минутах от UTC
 
 const vtdWorksLinkEl = document.querySelector('#vtd-works');
 const blinkerOutlineEl = document.querySelector('#blinker-outline');
 const blinkerCircleEl = document.querySelector('#blinker-circle');
-const vtdContactsBlockEl = document.querySelector('#vtd-contacts');
+const vtdTelEl = document.querySelectorAll('.vtd-tel');
+const vtdMailEl = document.querySelectorAll('.vtd-mail');
 
-function setTimeToZone(timeObject) {
+function getMoscowNowTime(currentNow) {
+	localTimeZone = currentNow.getTimezoneOffset();
+	localOffsetTime = localTimeZone - MOSCOW_TIME_ZONE;
+	return new Date(currentNow.getTime() + localOffsetTime * MINUTE);
+}
+
+function getWorkingTimeFromNowDay(day, timeObject) {
 	let time = new Date();
-	time.toLocaleString('ru-RU', { timeZone: 'Europe/Moscow' });
-	if (timeObject) time.setHours(...timeObject);
-	return time;
+	time.setDate(day);
+	time.setHours(...timeObject);
+	return time.getTime();
 }
 
 function setBlinker(color) {
@@ -21,40 +28,86 @@ function setBlinker(color) {
 		'blinker__circle blinker__circle--' + color;
 }
 
-let now = setTimeToZone();
-let workingBegin = 0;
-if (now.getDay == 0 || now.getDay == 6) {
-	//TODO выходные
-}
-else if (now.getDay() == 5) {
-	workingBegin = setTimeToZone([10, 0, 0]).getTime(); // 10:00
-} else {
-	workingBegin = setTimeToZone([8, 30, 0]).getTime(); // 8:30
-}
-now = now.getTime();
-const workingEnd = setTimeToZone([15, 30, 0]).getTime(); // 15:30
-const breakBegin = setTimeToZone([12, 0, 0]).getTime(); // 12:00
-const breakEnd = setTimeToZone([12, 30, 0]).getTime(); // 12:30
-
-if (
-	(now > workingBegin && now < breakBegin) ||
-	(now > breakEnd && now < workingEnd)
-) {
-	vtdWorksLinkEl.textContent = 'ВТД работает, звоните:';
-	setBlinker('blue');
-} else if (now > breakBegin && now < breakEnd) {
-	const rest = breakBegin - now;
-	vtdWorksLinkEl.textContent = `Перерыв еще ${rest.getHours()} : ${rest.getMinutes()}, пишите: `;
-	setBlinker('red');
-} else {
-	let rest = 0;
-	if (now > workingEnd) {
-		rest = workingBegin + DAY * HOUR * MINUTE * SECOND - now;
-	} else if (now < workingBegin) {
-		rest = workingBegin - now;
+function setText(text) {
+	switch (text) {
+		case 'weekend':
+			vtdWorksLinkEl.textContent = `ВТД: выходные, пишите:`;
+			break;
+		case 'open':
+			vtdWorksLinkEl.textContent = 'ВТД: работает, звоните:';
+			break;
+		case 'break':
+			vtdWorksLinkEl.textContent = `ВТД:Перерыв, пишите: `;
+			break;
+		case 'close':
+			vtdWorksLinkEl.textContent = `ВТД: закрыто до утра, пишите: `;
+			break;
 	}
-	vtdWorksLinkEl.textContent = `До открытия ${Math.floor(
-		rest / (HOUR * MINUTE * SECOND)
-	)} : ${Math.floor((rest / (MINUTE * SECOND)) % HOUR)} `;
-	setBlinker('red');
 }
+
+function setContacts(contact) {
+	if (contact == 'email') {
+		vtdTelEl.forEach((el) => el.classList.add('no-visible'));
+		vtdMailEl.forEach((el) => el.classList.remove('no-visible'));
+	} else if (contact == 'tel') {
+		vtdTelEl.forEach((el) => el.classList.remove('no-visible'));
+		vtdMailEl.forEach((el) => el.classList.add('no-visible'));
+	}
+}
+	const moscowNowTime = getMoscowNowTime(new Date());
+
+	if (moscowNowTime.getDay() == 0 || moscowNowTime.getDay() == 6) {
+		setText('weekend');
+		setBlinker('red');
+		setContacts('email');
+	} else {
+		let workingBegin = 0;
+
+		if (moscowNowTime.getDay() == 5) {
+			workingBegin = getWorkingTimeFromNowDay(
+				moscowNowTime.getDate(),
+				[10, 0, 0]
+			); // 10:00
+		} else {
+			workingBegin = getWorkingTimeFromNowDay(
+				moscowNowTime.getDate(),
+				[8, 30, 0]
+			); // 8:30
+		}
+
+		const workingEnd = getWorkingTimeFromNowDay(
+			moscowNowTime.getDate(),
+			[15, 30, 0]
+		); // 15:30
+		const breakBegin = getWorkingTimeFromNowDay(
+			moscowNowTime.getDate(),
+			[12, 0, 0]
+		); // 12:00
+		const breakEnd = getWorkingTimeFromNowDay(
+			moscowNowTime.getDate(),
+			[12, 30, 0]
+		); // 12:30
+		console.log(moscowNowTime, workingBegin, workingEnd, breakBegin, breakEnd);
+		const now = moscowNowTime.getTime();
+		if (
+			(now > workingBegin && now < breakBegin) ||
+			(now > breakEnd && now < workingEnd)
+		) {
+			setText('open');
+			setBlinker('blue');
+			setContacts('tel');
+		} else if (now > breakBegin && now < breakEnd) {
+			setText('break');
+			setBlinker('red');
+		} else {
+			setText('close');
+			setBlinker('red');
+		}
+	}
+
+// addEventListener('unload', () => {
+// 	clearInterval(idInterval)
+// })
+// addEventListener('beforeunload', () => {
+// 	clearInterval(idInterval)
+// })
